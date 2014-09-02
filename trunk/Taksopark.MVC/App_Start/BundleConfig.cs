32ee8eec76508a354f51a5b3dda4697ae24f27ee
komaniakvitalik@gmail.com <komaniakvitalik@gmail.com@8aa69a50-2309-e3b6-28de-71363f0c73f1>
2014-09-02 13:57:03 +0000

    Create site design. Create login popup and slideer show on start page.

--- trunk/Taksopark.MVC/App_Start/BundleConfig.cs
+++ trunk/Taksopark.MVC/App_Start/BundleConfig.cs
@@ -10,8 +10,16 @@ namespace Taksopark.MVC
                                                                       "~/Content/Layout/login-style.css"));
             bundles.Add(new StyleBundle("~/Content/Layout/about").Include("~/Content/Layout/About.css"));
             bundles.Add(new StyleBundle("~/Content/Layout/error-style").Include("~/Content/Layout/error-style.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.min.css",
+                                                                      "~/Content/bootstrap-responsive.css"));
+
             bundles.Add(new ScriptBundle("~/bundles/userinfo").Include(
                       "~/Scripts/app-scripts/userinfo-script.js"));
+            bundles.Add(new ScriptBundle("~/bundles/ready-script").Include("~/Scripts/app-scripts/ready.js", "~/Scripts/app-scripts/camera.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-1.10.2.min.js", "~/Scripts/bootstrap.min.js"));
         }
     }
 }
