b64e07afc7cc394a6b7d19ae6b3b76ab8fbf5862
Orest.Zamorylo <Orest.Zamorylo@gmail.com>
2014-09-14 22:34:41 +0000

    Created LogInModel.cs

---
+++ Taksopark.MVC/Models/LogInModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Taksopark.MVC.Models
+{
+    public class LogInModel
+    {
+        [Required(ErrorMessage = "Login Required:")]
+        [StringLength(50, ErrorMessage = "Less than 50 characters")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Password Required:")]
+        [DataType(DataType.Password)]
+        [DisplayName("Password:")]
+        [StringLength(30, ErrorMessage = "Less than 30 characters")]
+        public string Password { get; set; }
+    }
+}
