24ce812bc874d61852ea1363f0a7dea5ece127a0
zikkryp <zikkryp@gmail.com>
2014-09-15 16:16:37 +0000

    Merged projects

---
+++ Taksopark.WebForms/WebForms/Confirmation.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Taksopark.WebForms.Dispatcher
+{
+    public partial class Confirmation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void statusList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void submitButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void cancelButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void dropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Taksopark.WebForms/WebForms/Home.aspx.cs
+++ Taksopark.WebForms/WebForms/Home.aspx.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Routing;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Taksopark.WebForms.WebForms
+namespace Taksopark.WebForms.Dispatcher
 {
-    public partial class Admin : System.Web.UI.MasterPage
+    public partial class Home : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
