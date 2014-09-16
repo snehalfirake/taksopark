8e898bf0f10376944a039019a248adedfb235929
komaniakvitalik@gmail.com <komaniakvitalik@gmail.com@8aa69a50-2309-e3b6-28de-71363f0c73f1>
2014-09-16 18:34:10 +0000

    Server validation

--- trunk/Taksopark.MVC/Models/Contact.cs
+++ trunk/Taksopark.MVC/Models/Contact.cs
@@ -8,12 +8,19 @@ namespace Taksopark.MVC.Models
 {
     public class Contact
     {
-        [Required]
+        [Required(ErrorMessage = "Name can not be empty")]
+        [StringLength(50, ErrorMessage = "Name must be less than 50 characters")]
         public string Name { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Email can not be empty")]
         [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$",
+                                                ErrorMessage = "Email Format is wrong")]
+        [StringLength(50, ErrorMessage = "Email must be less than 50 characters")]
         public string Email { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Message can not be empty")]
+        [StringLength(3500, ErrorMessage = "Message than must be less 3500 characters")]
         public string Message { get; set; }
     }
 }
