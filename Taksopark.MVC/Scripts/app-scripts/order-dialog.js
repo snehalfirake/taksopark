9e19868f19d99ff51583c1a1abad942b5377730a
komaniakvitalik <komaniakvitalik@gmail.com>
2014-09-21 11:10:48 +0000

    added popup js for ordering

---
+++ Taksopark.MVC/Scripts/app-scripts/order-dialog.js
@@ -0,0 +1,39 @@
+﻿var query = $(function () {
+    $("#dialog").dialog({
+        autoOpen: false,
+        modal: true,
+        resizable: false,
+        buttons: [{
+            text: "Order",
+            click: function() {
+                $.ajax({
+                    url: "/Home/OrderTaxi",
+                    type: 'POST',
+                    dataType: 'json',
+                    cache: false,
+                    success: function() {
+                        alert("Thank you for using our ");
+                    },
+                    error: function() {
+                        alert("Somthing wrong!!! Please try again");
+                    }
+                });
+                $(this).dialog("close");
+            },
+},
+            {
+                text: "Cancel",
+                click: function() { 
+                    $(this).dialog("close"); 
+                }
+            }]
+    });
+
+    $("#orderTaxiButtonId").on("click", function () {
+        if (!(($("#placeFromTextBoxId").val() == "") && ($("#placeToTextBoxId").val() == "") && ($("#phoneTextBoxId").val() == ""))) {
+            $("#dialog").dialog("open");
+        } else {
+
+        }
+    });
+});
