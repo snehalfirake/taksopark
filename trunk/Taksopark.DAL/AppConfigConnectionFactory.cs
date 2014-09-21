29846f018aa54d379d99fa6a31a97010c385ea39
Orest.Zamorylo@gmail.com <Orest.Zamorylo@gmail.com@8aa69a50-2309-e3b6-28de-71363f0c73f1>
2014-09-21 14:27:23 +0000

    Added AppConfigConnectionFactory

---
+++ trunk/Taksopark.DAL/AppConfigConnectionFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace Taksopark.DAL
+{
+    public class AppConfigConnectionFactory : IAppConfigConnectionFactory
+    {
+        private readonly string _connectionString;
+        private const string ConnectionName = "ConnectionString";
+
+        public AppConfigConnectionFactory()
+        {
+            _connectionString = WebConfigurationManager.ConnectionStrings[ConnectionName].ConnectionString;
+        }
+
+        public SqlConnection Create()
+        {
+            var connection = new SqlConnection(_connectionString);
+            if (connection == null)
+            {
+                throw new ConfigurationErrorsException("Failed to create connection using provided connection name");
+            }
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception)
+            {
+                throw new DataException("Failed to connect to DB");
+            }
+            return connection;
+        }
+    }
+}
