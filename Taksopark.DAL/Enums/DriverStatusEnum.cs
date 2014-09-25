5db3f67360635dde3a1e6bb1f92e664b21047b14
Orest.Zamorylo <Orest.Zamorylo@gmail.com>
2014-09-25 13:44:59 +0000

    Added Enums folder in DAL

---
+++ Taksopark.DAL/Enums/DriverStatusEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taksopark.DAL.Enums
+{
+    public enum DriverStatusEnum
+    {
+        Free = 2,
+        Busy = 4
+    };
+}
---
+++ Taksopark.DAL/Enums/RequestStatusEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taksopark.DAL.Enums
+{
+    public enum RequestStatusEnum
+    {
+        Active = 2,
+        InProgress = 4,
+        Closed = 8,
+        Rejected = 32
+    };
+}
