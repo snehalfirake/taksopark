029c4080cf04e70e1631cd72d4bfe772103b41f6
Orest.Zamorylo <Orest.Zamorylo@gmail.com>
2014-09-25 13:05:56 +0000

    Changes to RouteConfig.cs

--- Taksopark.MVC/App_Start/RouteConfig.cs
+++ Taksopark.MVC/App_Start/RouteConfig.cs
@@ -10,6 +10,21 @@ namespace Taksopark.MVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Account/RegisterUser",
+                url: "register",
+                defaults: new { controller = "Account", action = "RegisterUser" }
+            );
+
+            routes.MapRoute(
+                name: "UserProfile/GetUserHistory",
+                url: "orders",
+                defaults: new { controller = "UserProfile", action = "GetUserHistory" }
+            );
+
+
+            //UserProfile/GetUserProfile
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
