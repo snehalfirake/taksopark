eaf6de070a003071e39711116c0d54be7b0f8708
komaniakvitalik@gmail.com <komaniakvitalik@gmail.com@8aa69a50-2309-e3b6-28de-71363f0c73f1>
2014-09-29 13:25:08 +0000

    Move logo to left side of header

--- trunk/Taksopark.MVC/Scripts/app-scripts/ready.js
+++ trunk/Taksopark.MVC/Scripts/app-scripts/ready.js
@@ -1,49 +1,17 @@
 ﻿var flag = false;
 
 $(document).ready(function () {
-    $("#toggle").hide();   
-    $("#show").click(function () {
-        if (!flag) {
-            flag = true;
-            $("#toggle").show("slide", { direction: "up" }, 500);
-        }
-        else {
-            flag = false;
-            $("#toggle").hide("slide", { direction: "up" }, 500);
+    $('#ExtendedFormOpenButtonId').click(function () {
+        var $extendedOrderPanel = $("#ExtendedOrderFormId");
+        if ($extendedOrderPanel.is(":visible")) {
+            $("#ExtendedOrderFormId").slideUp(400);
+            $("#ExtendedFormOpenButtonId").text("Extended \u25BC");
             $("#date-time").val("");
-            $('input:radio[name=service]').prop('checked', false);
+            $('input:radio[name=service]').prop("checked", false);
+        } else {
+            $("#ExtendedOrderFormId").slideDown(400);
+            $("#ExtendedFormOpenButtonId").text("Extended \u25B2");
         }
     });
     $("#dtBox").DateTimePicker();
 });
-
-
-all_images = new Array(
-"/Content/image/slide.jpg",
-"/Content/image/slide1.jpg",
-"/Content/image/slide2.jpg");
-var ImgNum = 0;
-var ImgLength = all_images.length - 1;
-var delay = 4500;
-var lock = false;
-var run;
-
-function chgImg(direction) {
-    if (document.images) {
-        ImgNum = ImgNum + direction;
-        if (ImgNum > ImgLength) { ImgNum = 0; }
-        if (ImgNum < 0) { ImgNum = ImgLength; }
-        document.slide_show.src = all_images[ImgNum];
-    }
-}
-
-function auto() {
-    if (lock == true) {
-        lock = false;
-        window.clearInterval(run);
-    }
-    else if (lock == false) {
-        lock = true;
-        run = setInterval("chgImg(1)", delay);
-    }
-}
